55a8a59c25e5d5129c2586e97a8916ba3ceec840
Borris <borris.wiria@gmail.com>
2022-10-06 23:02:40 +0400

    init

---
+++ apps/my-remix-app/app/routes/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from 'ui'
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to Remix</h1>
+      <Button onClick={() => console.log('clicked')}>Click me</Button>
+      <ul>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/blog'
+            rel='noreferrer'>
+            15m Quickstart Blog Tutorial test
+          </a>
+        </li>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/jokes'
+            rel='noreferrer'>
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
